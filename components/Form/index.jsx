0061eb5478e26904873ef23698f9d8e9b583fce9
dalemat <31269397+dalemat@users.noreply.github.com>
2020-12-03 09:55:31 +0100

    Update index.jsx

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -32,14 +32,14 @@ export default function Form({details, setDetails}){
         <>
         <form onSubmit={(e)=>e.preventDefault()} className="flex flex-wrap shadow md:py-16 rounded-md bg-white w-full p-6 mt-20 md:w-800 m-auto">
             <div className="md:flex md:flex-wrap md:w-full md:justify-between">
-                <InputField value={burnValue} handleChange={setBurnValue} col="red400" borderCol="red400border" labelText="Send USDT Token" textColor="red-400" tokenName="USDT" tokenValue={oldTokenBalance} />
+                <InputField value={burnValue} handleChange={setBurnValue} col="red400" borderCol="red400border" labelText="Send USDT Token wk1 $5/token" textColor="red-400" tokenName="USDT" tokenValue={oldTokenBalance} />
                 <div className="flex w-full justify-center pt-6 pb-4 transform rotate-90 md:rotate-0 md:w-1/5"><img className="h-6 mt-4" src="/swapicon.png" alt="swap" /></div>
-                <InputField value={mintValue} col="green400" borderCol="green400border" disabled labelText="Recieve DGOVn Token" textColor="greenish" tokenName="DGOVn Token" tokenValue={newTokenBalance} />
+                <InputField value={mintValue} col="green400" borderCol="green400border" disabled labelText="Recieve DGOVn Token wk2 $10/token" textColor="greenish" tokenName="DGOVn Token" tokenValue={newTokenBalance} />
             </div>
         </form>
         <div className="flex flex-wrap justify-between w-full px-6 mt-10 md:w-800 m-auto">
                 <Button onClick={handleApprove} text="Approve" bgActive="greenish" bgInactive="white" disabled={(enoughAllowance||oldTokenBalance<burnValue)} />
-                <Button onClick={handleBurn} disabled={(!enoughAllowance&&Number(burnValue)>oldTokenAllowance)||burnValue==""} text="Recieve DGOVn token" bgActive="greenish" bgInactive="white" />
+                <Button onClick={handleBurn} disabled={(!enoughAllowance&&Number(burnValue)>oldTokenAllowance)||burnValue==""} text="Get DGOVn token" bgActive="greenish" bgInactive="white" />
         </div>
 
     
